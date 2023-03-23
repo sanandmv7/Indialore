8e337b3394255eefdcdd92f4ea47ca6ad2d762fa
sanandmv7 <56066399+sanandmv7@users.noreply.github.com>
2023-03-24 00:05:17 +0530

    Add comments to store and marketplace

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,31 +1,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
+// Importing the Store contract
 import "./Store.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
+// Contract "IndialoreMarketplace"
 contract IndialoreMarketplace {
+    // Using the Strings library for string manipulation on uint256 variables
     using Strings for uint256;
-    mapping(address=>bool) public isSeller;
 
-    mapping(address=>Store) public sellerToStore;
+    // Mapping to keep track of which addresses are sellers
+    mapping(address => bool) public isSeller;
 
+    // Mapping to keep track of which seller owns which store
+    mapping(address => Store) public sellerToStore;
+
+    // Counter to keep track of the number of stores created
     uint256 currentStoreId = 0;
 
+    // Modifier to restrict access to functions to only sellers
     modifier onlySellers() {
         require(isSeller[msg.sender], "caller is not a seller");
         _;
     }
 
+    // Function to register a seller
     function registerSeller() external {
+        // Mark the caller's address as a seller
         isSeller[msg.sender] = true;
     }
 
+    // Function to create a new store for the caller
     function createStore(string memory _storeName) external onlySellers {
-        require(address(sellerToStore[msg.sender]) == address(0), "Only one store per seller is allowed");
+        // Ensure that the seller doesn't already have a store
+        require(
+            address(sellerToStore[msg.sender]) == address(0),
+            "Only one store per seller is allowed"
+        );
+        // Generate a unique store ID based on the current store ID counter
         string memory _storeId = string.concat("IL", currentStoreId.toString());
+        // Create a new Store contract with the given name and store ID
         Store store = new Store(_storeName, _storeId);
+        // Associate the new Store contract with the seller's address
         sellerToStore[msg.sender] = store;
+        // Increment the store ID counter
         currentStoreId++;
     }
 }
--- contracts/Store.sol
+++ contracts/Store.sol
@@ -1,29 +1,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
+// Importing the ERC721 and Ownable contracts from OpenZeppelin
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+// Contract "Store" inherits from ERC721 and Ownable
 contract Store is ERC721, Ownable {
+    // "Product" struct to represent a product for sale
     struct Product {
-        string name;
-        uint256 price;
-        address seller;
+        string name; // Name of the product
+        uint256 price; // Price of the product
+        address seller; // Address of the seller who listed the product for sale
     }
 
-    mapping(uint256=>Product) tokenIdToProducts;
+    // Mapping from token IDs to the products they represent
+    mapping(uint256 => Product) tokenIdToProducts;
 
-    constructor(string memory _storeName, string memory _storeId) ERC721(_storeName, _storeId) {}
+    // Constructor takes in a store name and ID and passes them to the ERC721 constructor
+    constructor(
+        string memory _storeName,
+        string memory _storeId
+    ) ERC721(_storeName, _storeId) {}
 
-    function listProductForSale(address _to, uint256 _tokenId, string memory _name, uint256 _price) public onlyOwner {
+    // Function that allows the contract owner to list a new product for sale
+    function listProductForSale(
+        address _to,
+        uint256 _tokenId,
+        string memory _name,
+        uint256 _price
+    ) public onlyOwner {
+        // Create a new Product struct with the given name, price, and seller address
         Product memory _item = Product(_name, _price, _to);
+        // Associate the product with the given token ID in the mapping
         tokenIdToProducts[_tokenId] = _item;
+        // Mint a new ERC721 token to the given address
         _safeMint(_to, _tokenId);
     }
 
+    // Function that allows a user to purchase a product with the given token ID by sending the required amount of Ether
     function purchaseProduct(uint256 _tokenId) public payable {
+        // Retrieve the product associated with the given token ID
         Product memory _item = tokenIdToProducts[_tokenId];
+        // Check that the amount of Ether sent by the buyer matches the price of the product
         require(msg.value == _item.price, "price doesn't match");
+        // Transfer ownership of the ERC721 token from the seller to the buyer
         _transfer(_item.seller, msg.sender, _tokenId);
     }
 }
