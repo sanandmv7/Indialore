10e10b4497cf5074d6446bfbaeee867ff1dc1ef2
sanandmv7 <56066399+sanandmv7@users.noreply.github.com>
2023-03-28 20:32:24 +0530

    Add pages

---
+++ indialore-frontend/src/Components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './Header.css';
+
+function Header() {
+    return (
+        <div className='headerParentDiv'>
+            <section id="header">
+                <img src="../assets/logo.png" class = "logo" alt="" />
+                
+                <div>
+                    <ul id="navbar">
+                        <li><a className="active" href="index.html">Home</a></li>
+                        <li><a href="shop.html">Shop</a></li>
+                        <li><a href="about.html">About Us</a></li>
+                        <li><a href="../Login/login.html">Login</a></li>
+                        <li><a href="cart.html"><i class="fa-duotone fa-cart-shopping"></i></a></li>
+                        
+                    </ul>
+                </div>
+            </section>
+        </div>
+        
+    );
+}
+
+export default Header;
---
+++ indialore-frontend/src/Components/Posts/Posts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Post.css';
+
+function Posts() {
+
+  return (
+    <div></div>
+  );
+}
+
+export default Posts;
