0c5e215ff0ea6b42ef9af920eaa0f3b8c8f822b9
sanandmv7 <56066399+sanandmv7@users.noreply.github.com>
2023-04-20 20:06:05 +0530

    Implement newsletter email collection

--- indialore-frontend/src/Components/Newsletter/Newsletter.js
+++ indialore-frontend/src/Components/Newsletter/Newsletter.js
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { FirebaseContext } from "../../contexts/UserContext";
 
 import "./Newsletter.css";
 
 function Newsletter() {
+  const [input, setInput] = useState("");
+  const [message, setMessage] = useState("");
+
+  const { firebase } = useContext(FirebaseContext);
+
+  const inputHandler = (e) => {
+    setInput(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (input) {
+      // console.log(input);
+
+      // add to firebase
+      firebase
+        .firestore()
+        .collection("emails")
+        .add({
+          email: input,
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+
+      // reset input
+      setInput("");
+
+      // set thank you message
+      setMessage("Thank you for subscribing!");
+
+      setTimeout(() => {
+        setMessage("");
+      }, 3000);
+    }
+  };
+
   return (
     <section id="newsletter" className="section-p1 section-m1">
       <div className="newstext">
@@ -11,10 +50,18 @@ function Newsletter() {
           Get E-mail updates about out latest shop and{" "}
           <span>special offers.</span>.
         </p>
+        <p>{message}</p>
       </div>
       <div className="form">
-        <input type="text" placeholder="Enter your email address" />
-        <button className="normal-button">Sign Up</button>
+        <input
+          type="text"
+          placeholder="Enter your email address"
+          onChange={inputHandler}
+          value={input}
+        />
+        <button className="normal-button" onClick={submitHandler}>
+          Sign Up
+        </button>
       </div>
     </section>
   );
